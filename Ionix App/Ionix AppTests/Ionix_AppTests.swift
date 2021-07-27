e52d06253778125818592856d6dad44b9530c287
Carlos Villamizar <>
2021-07-27 09:59:48 -0400

    Initial project

---
+++ Ionix App/Ionix AppTests/Ionix_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Ionix_AppTests.swift
+//  Ionix AppTests
+//
+//  Created by Ruben Duarte on 27/7/21.
+//
+
+import XCTest
+@testable import Ionix_App
+
+class Ionix_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
